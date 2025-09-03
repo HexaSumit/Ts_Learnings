9e80bba2193a2380bce0f35f96db829d6304ab65
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-03 08:33:57 +0530

    Add Counter to learn use state

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count,setCount]=useState<number>(0)
+  const [count, setCount] = useState<number>(0) //typed as number 
+  
 
+  const handleIncrease=()=>{
+    setCount(count+1)
+  }
+  const handleDecrease=()=>{
+    if(count>0){
+      setCount(count-1)
+    }
+  }
   return (
     <>
-    <div>
-      <p>Clicked {count} times!</p><br />
-      <button>+</button>
-    </div>
+      <div>
+        <p>Clicked {count} times!</p><br />
+        <div >
+          <button className=' mr-10' onClick={handleIncrease}>Increase +</button>
+          <button onClick={handleDecrease}>Decrease -</button>
+        </div>
+      </div>
     </>
   )
 }
